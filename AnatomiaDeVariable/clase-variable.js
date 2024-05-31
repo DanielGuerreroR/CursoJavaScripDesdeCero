f7a21eee333964f29df036af3d8f51c56b3f1063
Daniel Guerrero <dansan_2002@hotmail.com>
2024-05-31 17:19:04 -0500

    clase en la que se vio las partes de una variable, como se declara, y buenas practicas

---
+++ AnatomiaDeVariable/clase-variable.js
@@ -0,0 +1,20 @@
+let cajaDeAndy = "Woody";
+//para poder ver la variable
+console.log(cajaDeAndy);
+
+//lo NO permitido
+let c = "Woody";
+let cda = "Woody";
+let pcAndy = "Woody";
+//tiene que ser lo mas descriptiblo posible
+//si tiene que ser largo, es mejor para q se entienda
+let c, cda;
+pcAndy; //tiene que ser ordenado
+
+//lo permitido
+let primerTrasteoDeAndy = "Woody";
+//abreviaciones como url o id estan permitidas ya que son conocidas
+let urlDelUsuario = "www.google.com";
+let idDelUsuario = "123456";
+//si tenemos varias bariables lo ideal es ordenado y que esten una tras otra,
+//como al inicio del codigo
