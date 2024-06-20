a78a77331fd946d5a096b14c89d8d653576e8c88
Daniel Guerrero <dansan_2002@hotmail.com>
2024-06-20 12:01:38 -0500

    se vio otra parte  de strings, viendo los caracteres de escape, el como escribir strings largos y con saltos de linea de diferente forma y que es string literals y string value

--- CreacionStrings/clase-string.js
+++ CreacionStrings/clase-string.js
@@ -42,3 +42,47 @@ const hobbies = "Mis hobbies son: ".concat(
 	"."
 );
 console.log(hobbies);
+
+//Caracteres de escape
+//const whatDoIDo= 'I'm Software Engineer'
+//Marca un error ya que las comillas simples se cierran antes y no permiten leer el string
+
+//1. Escape alternativo
+const escapeAlternativo = "I'm Software Engineer";
+
+//2. Barra invertida
+const escapeBarraInvertida = "I'm Software Engineer";
+
+//3. Template Literals
+const escapeComillaInvertida = `I'm Software Engineer`;
+
+//Escribir Strings largos
+/*
+Las rosas son rojas,
+Las violetas son azules,
+Caracter inesperado,
+En la linea 86
+*/
+const poema =
+	"Las rosas son rojas,\n" +
+	"Las violetas son azules,\n" +
+	"Caracter inesperado,\n" +
+	"En la linea 86";
+console.log(poema);
+//para escribir el poema sin concatenaciones
+const poema2 =
+	"Las rosas son rojas,\n\
+Las violetas son azules,\n\
+Caracter inesperado,\n\
+En la linea 86";
+console.log(poema2);
+//el poema sin saltos de linea \n\ solo con comillas invertidas
+const poema3 = `Las rosas son rojas,
+Las violetas son azules,
+Caracter inesperado,
+En la linea 86`;
+console.log(poema3);
+
+/*String Literals = cuando escribimos un string lo hacemos con comillas,
+eso es un string literals
+String Values = es cuando vemos un resultado en pantalla de esos resultados  */
